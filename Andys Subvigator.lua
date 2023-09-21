4376a5116cd4c7335c53fbb800b635f906726bc6
andymees <116795369+andymees@users.noreply.github.com>
2023-09-21 11:08:55 +0100

    remove subs limit
    The script uses leading zero padding on subtitle item numbers to allow for alphabetic sorting and also uses that number to filter visibility of subsequent text search hits. The original version used fixed padding to 3 digits... this version just adds as many zeros as needed.

--- Andys Subvigator.lua
+++ Andys Subvigator.lua
@@ -6,19 +6,20 @@ local subTrack = 1
 local matchType = {MatchContains = true }
 
 -- Setup 'Subvigator' window
-svg = disp:AddWindow({ID = 'MyWin', WindowTitle = "Andy's Subvigator", Geometry = {100, 100, 380, 700}, Spacing = 0, ui.VGroup{ui.VGap(2), ui.HGroup{Weight = 0, ui.HGap(10), ui.Label{ID = 'Label', Text = 'Filter', Weight = 0.05}, ui.LineEdit{ID = 'SearchText', PlaceholderText = 'Search Text Filter', Weight = 0.9}, ui:ComboBox{ID = 'SearchType', Weight = 0.05}, }, ui.VGap(2), ui.Tree{ID = 'Tree', SortingEnabled = true, Events = {ItemClicked = true, }, }, ui.VGap(2), ui.HGroup{Weight = 0, ui:ComboBox{ID = 'SearchTrack', Weight = 0.3, Events ={Activated = true,}}, ui.HGap(10), ui.Button{ID = 'refreshBtn', Text = 'Refresh'},ui.HGap(10)}, }, })
-itm = svg:GetItems()
+subvigator = disp:AddWindow({ID = 'MyWin', WindowTitle = "Andy's Subvigator", Geometry = {100, 100, 380, 700}, Spacing = 0, ui.VGroup{ui.VGap(2), ui.HGroup{Weight = 0, ui.HGap(10), ui.Label{ID = 'Label', Text = 'Filter', Weight = 0.05}, ui.LineEdit{ID = 'SearchText', PlaceholderText = 'Search Text Filter', Weight = 0.9}, ui:ComboBox{ID = 'SearchType', Weight = 0.05}, }, ui.VGap(2), ui.Tree{ID = 'Tree', SortingEnabled = true, Events = {ItemClicked = true, }, }, ui.VGap(2), ui.HGroup{Weight = 0, ui:ComboBox{ID = 'SearchTrack', Weight = 0.3, Events ={Activated = true,}}, ui.HGap(10), ui.Button{ID = 'refreshBtn', Text = 'Refresh'},ui.HGap(10)}, }, })
+itm = subvigator:GetItems()
 
 projectManager = resolve:GetProjectManager()
 project = projectManager:GetCurrentProject()
 timeline = project:GetCurrentTimeline()
 framerate = timeline:GetSetting('timelineFrameRate')
+trackItems = timeline:GetItemListInTrack('subtitle', subTrack)
+subsCount = #trackItems
+zeroPad = '%0'..string.len(subsCount)..'d'
 
 -- Add combo box options for track choice
 trackCount = timeline:GetTrackCount('subtitle')
 for track = 0, trackCount-1 do itm.SearchTrack:AddItem('ST '..track+1) end
-trackItems = timeline:GetItemListInTrack('subtitle', subTrack)
-subsCount = #trackItems
 
 -- Add combo box items for search choice
 itm.SearchType:AddItem('Contains')
@@ -34,15 +35,14 @@ itm.Tree:SetHeaderItem(hdr)
 
 -- Setup column widths
 itm.Tree.ColumnCount = 2
-itm.Tree.ColumnWidth[0] = 48
+itm.Tree.ColumnWidth[0] = 58
 itm.Tree.ColumnWidth[1] = 290
 
--- Define row function
+-- Define function to add rows
 function populateTable(hide)
-    -- Add an new row entries to the list
     for row = 1, subsCount do
         itRow = itm.Tree:NewItem()
-        itRow.Text[0] = string.format('%03d', row)
+        itRow.Text[0] = string.format(zeroPad, row)
         itRow.Text[1] = trackItems[row]:GetName()
         itRow.Text[2] = tostring(trackItems[row]:GetStart())
         itm.Tree:AddTopLevelItem(itRow)
@@ -57,19 +57,26 @@ populateTable(0)
 itm.Tree:SortItems(0,'AscendingOrder')
 
 -- Move timeline playhead
-function svg.On.Tree.ItemClicked(ev)
+function subvigator.On.Tree.ItemClicked(ev)
     frameval = ev.item.Text[2]
+    print('frameval '..frameval)
     secs = frameval/framerate
+    print('frame val in secs '..secs)
     hours   = math.floor(secs / 3600) % 24
+    print('hours '..hours)
     minutes = math.floor(secs / 60) % 60
+    print('minutes '..minutes)
     seconds = secs % 60
+    print('seconds '..seconds)
     frames = frameval % framerate
+    print('frames '..frames)
     timecode = string.format('%02d', hours)..':'..string.format('%02d', minutes)..':'..string.format('%02d', seconds)..':'..string.format('%02d', frames)
+    print('timecode '..timecode)
     timeline:SetCurrentTimecode(timecode)
 end
 
 -- Filter for search text
-function svg.On.SearchText.TextChanged(ev)
+function subvigator.On.SearchText.TextChanged(ev)
     mySearchText = itm.SearchText.Text
     hits = itm.Tree:FindItems(mySearchText, matchType, 1)
     if (mySearchText == '') then for row = 0, subsCount-1 do itm.Tree:TopLevelItem(row):SetHidden(false) end
@@ -80,37 +87,35 @@ function svg.On.SearchText.TextChanged(ev)
 end
 
 -- Change search type
-function svg.On.SearchType.CurrentIndexChanged(ev)
+function subvigator.On.SearchType.CurrentIndexChanged(ev)
     if itm.SearchType.CurrentIndex == 0 then matchType = {MatchContains = true }
     elseif itm.SearchType.CurrentIndex == 1 then matchType = {MatchExactly = true }
     elseif itm.SearchType.CurrentIndex == 2 then matchType = {MatchStartsWith = true }
     elseif itm.SearchType.CurrentIndex == 3 then matchType = {MatchEndsWith = true } end
-    svg.On.SearchText.TextChanged()
+    subvigator.On.SearchText.TextChanged()
 end
 
 -- Change search track
-function svg.On.SearchTrack.Activated(ev)
+function subvigator.On.SearchTrack.Activated(ev)
     subTrack = itm.SearchTrack.CurrentIndex +1
-    if (not(subTrack == lastTrack)) then lastTrack = subTrack; svg.On.refreshBtn.Clicked() end
+    if (not(subTrack == lastTrack)) then lastTrack = subTrack; subvigator.On.refreshBtn.Clicked() end
 end
 
 -- Refresh table
-function svg.On.refreshBtn.Clicked(ev)
+function subvigator.On.refreshBtn.Clicked(ev)
     project = projectManager:GetCurrentProject()
     timeline = project:GetCurrentTimeline()
-    trackItems = timeline:GetItemListInTrack('subtitle', subTrack)
     framerate = timeline:GetSetting('timelineFrameRate')
     trackCount = timeline:GetTrackCount('subtitle')
+    trackItems = timeline:GetItemListInTrack('subtitle', subTrack)
+    subsCount = #trackItems
+    zeroPad = '%0'..string.len(subsCount)..'d'
     
     itm.SearchTrack:Clear()
     for track = 0, trackCount-1 do itm.SearchTrack:AddItem('ST '..track+1) end
-    trackItems = timeline:GetItemListInTrack('subtitle', subTrack)
-    subsCount = #trackItems
 
-	-- Remove all rows
+    -- Remove all rows and repopulate table
 	itm.Tree:Clear()
-
-    -- Add rows (keep hidden items)
     populateTable(1)
     
     -- Apply default sort
@@ -126,8 +131,8 @@ function svg.On.refreshBtn.Clicked(ev)
 end
 
 -- The window was closed
-function svg.On.MyWin.Close(ev) disp:ExitLoop() end
+function subvigator.On.MyWin.Close(ev) disp:ExitLoop() end
 
-svg:Show()
+subvigator:Show()
 disp:RunLoop()
-svg:Hide()
+subvigator:Hide()
