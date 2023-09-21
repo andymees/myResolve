5f184917920a61c4af0ee02f2cabc1b7ced44e55
andymees <116795369+andymees@users.noreply.github.com>
2023-09-21 16:28:39 +0100

    incorporates Roger Magnusson's timecode utility functions
    replaces my original, very limited, frames to timecode function with Roger Magnusson's full Libavutil.lua routines, which support both fractional and drop frame timebases.
    
    https://rogmag.github.io/downloads/creating_davinci_resolve_scripts/scripts/utility/libavutil/Libavutil.lua

--- Andys Subvigator.lua
+++ Andys Subvigator.lua
@@ -1,3 +1,209 @@
+--------------------------------------------------
+-- Roger Magnusson's Timecode Utility Functions --
+--------------------------------------------------
+
+--[[
+
+MIT License
+
+Copyright (c) 2023 Roger Magnusson
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+]]
+
+local luaresolve, libavutil
+
+luaresolve = 
+{
+	frame_rates =
+	{
+		get_fraction = function(self, frame_rate_string_or_number)
+			local frame_rate = tonumber(tostring(frame_rate_string_or_number))
+			-- These are the frame rates that DaVinci Resolve Studio supports as of version 18
+			local frame_rates = { 16, 18, 23.976, 24, 25, 29.97, 30, 47.952, 48, 50, 59.94, 60, 72, 95.904, 96, 100, 119.88, 120 }
+
+			for _, current_frame_rate in ipairs (frame_rates) do
+				if current_frame_rate == frame_rate or math.floor(current_frame_rate) == frame_rate then
+					local is_decimal = current_frame_rate % 1 > 0
+					local denominator = iif(is_decimal, 1001, 100)
+					local numerator = math.ceil(current_frame_rate) * iif(is_decimal, 1000, denominator)
+					return { num = numerator, den = denominator }
+				end
+			end
+
+			return nil, string.format("Invalid frame rate: %s", frame_rate_string_or_number)
+		end,
+
+		get_decimal = function(self, frame_rate_string_or_number)
+			local fractional_frame_rate, error_message = self:get_fraction(frame_rate_string_or_number)
+			
+			if fractional_frame_rate ~= nil then
+				return tonumber(string.format("%.3f", fractional_frame_rate.num / fractional_frame_rate.den))
+			else
+				return nil, error_message
+			end
+		end,
+	},
+
+	load_library = function(name_pattern)
+		local files = bmd.readdir(fu:MapPath("FusionLibs:"..iif(ffi.os == "Windows", "", "../"))..name_pattern)
+		assert(#files == 1 and files[1].IsDir == false, string.format("Couldn't find exact match for pattern \"%s.\"", name_pattern))
+		return ffi.load(files.Parent..files[1].Name)
+	end,
+
+	frame_from_timecode = function(self, timecode, frame_rate)
+		return libavutil:av_timecode_init_from_string(timecode, self.frame_rates:get_fraction(frame_rate)).start
+	end,
+
+	timecode_from_frame = function(self, frame, frame_rate, drop_frame)
+		return libavutil:av_timecode_make_string(0, frame, self.frame_rates:get_decimal(frame_rate),
+		{
+			AV_TIMECODE_FLAG_DROPFRAME = drop_frame == true or drop_frame == 1 or drop_frame == "1",
+			AV_TIMECODE_FLAG_24HOURSMAX = true,
+			AV_TIMECODE_FLAG_ALLOWNEGATIVE = false
+		})
+	end
+}
+
+libavutil = 
+{
+	library = luaresolve.load_library(iif(ffi.os == "Windows", "avutil*.dll", iif(ffi.os == "OSX", "libavutil*.dylib", "libavutil.so"))),
+
+	demand_version = function(self, version)
+		local library_version = self:av_version_info()
+
+		return (library_version.major > version.major)
+			or (library_version.major == version.major and library_version.minor > version.minor)
+			or (library_version.major == version.major and library_version.minor == version.minor and library_version.patch > version.patch)
+			or (library_version.major == version.major and library_version.minor == version.minor and library_version.patch == version.patch)
+	end,
+
+	set_declarations = function()
+		ffi.cdef[[
+			enum AVTimecodeFlag {
+				AV_TIMECODE_FLAG_DROPFRAME      = 1<<0, // timecode is drop frame
+				AV_TIMECODE_FLAG_24HOURSMAX     = 1<<1, // timecode wraps after 24 hours
+				AV_TIMECODE_FLAG_ALLOWNEGATIVE  = 1<<2, // negative time values are allowed
+			};
+
+			struct AVRational { int32_t num; int32_t den; };
+			struct AVTimecode { int32_t start; enum AVTimecodeFlag flags; struct AVRational rate; uint32_t fps; };
+
+			char* av_timecode_make_string(const struct AVTimecode* tc, const char* buf, int32_t framenum);
+			int32_t av_timecode_init_from_string(struct AVTimecode* tc, struct AVRational rate, const char* str, void* log_ctx);
+
+			char* av_version_info (void);
+		]]
+	end,
+
+	av_timecode_make_string = function(self, start, frame, fps, flags)
+		local function bor_number_flags(enum_name, flags)
+			local enum_value = 0    
+	
+			if (flags) then
+				for key, value in pairs(flags) do
+					if (value == true) then
+						enum_value = bit.bor(enum_value, tonumber(ffi.new(enum_name, key)))
+					end
+				end
+			end
+
+			return enum_value;
+		end
+
+		local tc = ffi.new("struct AVTimecode",
+		{
+			start = start,
+			flags = bor_number_flags("enum AVTimecodeFlag", flags),
+			fps = math.ceil(luaresolve.frame_rates:get_decimal(fps))
+		})
+
+		if (flags.AV_TIMECODE_FLAG_DROPFRAME and fps > 60 and (fps % (30000 / 1001) == 0 or fps % 29.97 == 0))
+			and (not self:demand_version( { major = 4, minor = 4, patch = 0 } ))
+		then
+			-- Adjust for drop frame above 60 fps (not necessary if BMD upgrades to libavutil-57 or later)
+			frame = frame + 9 * tc.fps / 15 * (math.floor(frame / (tc.fps * 599.4))) + (math.floor((frame % (tc.fps * 599.4)) / (tc.fps * 59.94))) * tc.fps / 15
+		end
+
+		local timecodestring = ffi.string(self.library.av_timecode_make_string(tc, ffi.string(string.rep(" ", 16)), frame))
+	
+		if (#timecodestring > 0) then
+			local frame_digits = #tostring(math.ceil(fps) - 1)
+
+			-- Fix for libavutil where it doesn't use leading zeros for timecode at frame rates above 100
+			if frame_digits > 2 then
+				timecodestring = string.format("%s%0"..frame_digits.."d", timecodestring:sub(1, timecodestring:find("[:;]%d+$")), tonumber(timecodestring:match("%d+$")))
+			end
+
+			return timecodestring
+		else
+			return nil
+		end
+	end,
+
+	av_timecode_init_from_string = function(self, timecode, frame_rate_fraction)
+		local tc = ffi.new("struct AVTimecode")
+		local result = self.library.av_timecode_init_from_string(tc, ffi.new("struct AVRational", frame_rate_fraction), timecode, ffi.new("void*", nil))
+	
+		if (result == 0) then
+			return
+			{
+				start = tc.start,
+				flags =
+				{
+					AV_TIMECODE_FLAG_DROPFRAME = bit.band(tc.flags, ffi.C.AV_TIMECODE_FLAG_DROPFRAME) == ffi.C.AV_TIMECODE_FLAG_DROPFRAME,
+					AV_TIMECODE_FLAG_24HOURSMAX = bit.band(tc.flags, ffi.C.AV_TIMECODE_FLAG_24HOURSMAX) == ffi.C.AV_TIMECODE_FLAG_24HOURSMAX,
+					AV_TIMECODE_FLAG_ALLOWNEGATIVE = bit.band(tc.flags, ffi.C.AV_TIMECODE_FLAG_ALLOWNEGATIVE) == ffi.C.AV_TIMECODE_FLAG_ALLOWNEGATIVE,
+				},
+				rate =
+				{
+					num = tc.rate.num,
+					den = tc.rate.den
+				},
+				fps = tc.fps
+			}
+		else
+			error("avutil error code: "..result)
+		end
+	end,
+
+	av_version_info = function(self)
+		local version = ffi.string(self.library.av_version_info())
+
+		return 
+		{
+			major = tonumber(version:match("^%d+")),
+			minor = tonumber(version:match("%.%d+"):sub(2)),
+			patch = tonumber(version:match("%d+$"))
+		}
+	end,
+}
+
+libavutil.set_declarations()
+
+
+
+------------------------
+-- Andy's Subvigator  --
+------------------------
+
 --[[MIT License
 
 Copyright (c) 2023 andymees
@@ -18,7 +224,9 @@ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
 AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.]]
+SOFTWARE.
+
+]]
     
 local ui = fu.UIManager
 local disp = bmd.UIDispatcher(ui)
@@ -27,9 +235,10 @@ local lastTrack = 1
 local subTrack = 1
 local matchType = {MatchContains = true }
 local requiresReturn = true
+local dropFrame = false
 
 -- Setup 'Subvigator' window
-subvigator = disp:AddWindow({ID = 'MyWin', WindowTitle = "Andy's Subvigator", Geometry = {100, 100, 380, 700 }, Spacing = 0, ui.VGroup{ui.VGap(2), ui.HGroup{Weight = 0, ui.HGap(10), ui.Label{ID = 'Label', Text = 'Filter', Weight = 0.05 }, ui.LineEdit{ID = 'SearchText', PlaceholderText = 'Search Text Filter', Weight = 0.9, Events = {TextChanged = true, ReturnPressed = true }, }, ui:ComboBox{ID = 'SearchType', Weight = 0.05 }, }, ui.HGroup{Weight = 0, ui.HGap(24), ui:CheckBox{ID = 'useDynamic', Text = 'Use dynamic filtering' }, }, ui.VGap(0), ui.Tree{ID = 'Tree', SortingEnabled = true, Events = {ItemClicked = true }, }, ui.VGap(2), ui.HGroup{Weight = 0, ui:ComboBox{ID = 'SearchTrack', Weight = 0.3, Events = {Activated = true }, }, ui.HGap(10), ui.Button{ID = 'refreshBtn', Text = 'Refresh' }, ui.HGap(10) }, }, })
+subvigator = disp:AddWindow({ID = 'MyWin', WindowTitle = "Andy's Subvigator", Geometry = {100, 100, 380, 700 }, Spacing = 0, ui.VGroup{ui.VGap(2), ui.HGroup{Weight = 0, ui.HGap(10), ui.Label{ID = 'Label', Text = 'Filter', Weight = 0.05 }, ui.LineEdit{ID = 'SearchText', PlaceholderText = 'Search Text Filter', Weight = 0.9, Events = {TextChanged = true, ReturnPressed = true }, }, ui:ComboBox{ID = 'SearchType', Weight = 0.05 }, }, ui.HGroup{Weight = 0, ui.HGap(24), ui:CheckBox{ID = 'useDynamic', Text = 'Use dynamic filtering' }, ui:CheckBox{ID = 'useDrop', Text = 'Use DF navigation' }, }, ui.VGap(0), ui.Tree{ID = 'Tree', SortingEnabled = true, Events = {ItemClicked = true }, }, ui.VGap(2), ui.HGroup{Weight = 0, ui:ComboBox{ID = 'SearchTrack', Weight = 0.3, Events = {Activated = true }, }, ui.HGap(10), ui.Button{ID = 'refreshBtn', Text = 'Refresh' }, ui.HGap(10) }, }, })
 itm = subvigator:GetItems()
 
 projectManager = resolve:GetProjectManager()
@@ -63,7 +272,6 @@ itm.Tree.ColumnWidth[1] = 280
 
 -- Define function to add rows
 function populateTable(hide)
-    print('populate table')
     for row = 1, subsCount do
         itRow = itm.Tree:NewItem()
         itRow.Text[0] = string.format(zeroPad, row)
@@ -82,26 +290,11 @@ itm.Tree:SortItems(0,'AscendingOrder')
 
 -- Move timeline playhead
 function subvigator.On.Tree.ItemClicked(ev)
-    frameval = ev.item.Text[2]
-    print('frameval '..frameval)
-    secs = frameval/framerate
-    print('frame val in secs '..secs)
-    hours   = math.floor(secs / 3600) % 24
-    print('hours '..hours)
-    minutes = math.floor(secs / 60) % 60
-    print('minutes '..minutes)
-    seconds = secs % 60
-    print('seconds '..seconds)
-    frames = frameval % framerate
-    print('frames '..frames)
-    timecode = string.format('%02d', hours)..':'..string.format('%02d', minutes)..':'..string.format('%02d', seconds)..':'..string.format('%02d', frames)
-    print('timecode '..timecode)
-    timeline:SetCurrentTimecode(timecode)
+    timeline:SetCurrentTimecode(luaresolve:timecode_from_frame(tonumber(ev.item.Text[2]), framerate, dropFrame))
 end
 
 -- Filter for search text
 function subvigator.On.SearchText.ReturnPressed(ev)-- TextChanged(ev)
-    print('perform text filter')
     mySearchText = itm.SearchText.Text
     hits = itm.Tree:FindItems(mySearchText, matchType, 1)
     if (mySearchText == '') then for row = 0, subsCount-1 do itm.Tree:TopLevelItem(row):SetHidden(false) end
@@ -111,9 +304,8 @@ function subvigator.On.SearchText.ReturnPressed(ev)-- TextChanged(ev)
     end
 end
 
--- Filter for search text
+-- Filter for search text - conditional control
 function subvigator.On.SearchText.TextChanged(ev)-- TextChanged(ev)
-    print('text changed')
     if (requiresReturn) then else subvigator.On.SearchText.ReturnPressed() end
 end
 
@@ -132,10 +324,15 @@ function subvigator.On.SearchTrack.Activated(ev)
     if (not(subTrack == lastTrack)) then lastTrack = subTrack; subvigator.On.refreshBtn.Clicked() end
 end
 
--- Change filtering method
+-- Toggle dynamic filtering
 function subvigator.On.useDynamic.Clicked(ev)
     requiresReturn = not(itm.useDynamic.Checked)
     subvigator.On.SearchText.TextChanged()
+end
+
+-- Toggle drop frame status
+function subvigator.On.useDrop.Clicked(ev)
+    dropFrame = itm.useDrop.Checked
 end
 
 -- Refresh table
