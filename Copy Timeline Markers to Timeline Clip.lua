c3d1f3bc62bf10009f835baffcfee9a4940c66bb
andymees <116795369+andymees@users.noreply.github.com>
2024-07-11 00:00:22 +0100

    Update Copy Timeline Markers to Timeline Clip.lua
    adds description

--- Copy Timeline Markers to Timeline Clip.lua
+++ Copy Timeline Markers to Timeline Clip.lua
@@ -1,3 +1,16 @@
+--[[ There's currently no failsafe method to programmatically identify a timline's corresponding media pool item,
+so before running this script you should use the 'Find Current Timeline in Media Pool' menu function.
+That will/should ensure the correct bin is open and thus the script finds the right media pool item.
+
+For reference, the script simply looks in the 'current' bin and finds the media pool 'timeline' item with the timeline name.
+Then, for each of the active timeline's timeline markers, the script adds a 'clip' marker to the media pool item. 
+
+Note 1: if any clip marker already exists at any of the timeline marker offsets, the original clip marker is preserved.
+Note 2: type 'Timeline' is a language specific variable, so you may need to localised for your language. 
+
+Hope it's useful, Andy Mees]]
+
+
 local function GetMatchingMediaPoolItem(tl)
     -- Get media pool item list for current bin
     items = project:GetMediaPool():GetCurrentFolder():GetClipList()
@@ -36,6 +49,6 @@ mediapoolTimeline = GetMatchingMediaPoolItem(timeline)
 timelineMarkers = timeline:GetMarkers()
 
 -- Add those markers to the matching media pool item
-for marker_frame, marker in pairs(timelineMarkers) do
-    mediapoolTimeline:AddMarker(marker_frame, marker.color, marker.name, marker.note, marker.duration, marker.customData)
+for marker_offset, marker in pairs(timelineMarkers) do
+    mediapoolTimeline:AddMarker(marker_offset, marker.color, marker.name, marker.note, marker.duration, marker.customData)
 end
